46b9b78bb3f9b3f1f68b2c21599ae0c81bbd684c
Facundo Cristaldo <facundocristaldo146@gmail.com>
2025-09-25 11:20:11 -0300

    rutas agregadas y modularizadas en el index.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,14 +7,29 @@ import {
   updateProfile,
 } from "../controllers/auth.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-
+import {
+  registerValidations,
+  updateProfileAuthValidations,
+} from "../middlewares/validations/auth.validations.js";
+import { applyValidations } from "../middlewares/validator.js";
 export const authRoutes = Router();
 
-authRoutes.post("/auth/register", register);
+authRoutes.post(
+  "/auth/register",
+  registerValidations,
+  applyValidations,
+  register
+);
 authRoutes.post("/auth/login", login);
 
-authRoutes.use(authMiddleware);
+// authRoutes.use(authMiddleware);
 
-authRoutes.get("/auth/profile", getProfile);
-authRoutes.put("/auth/profile", updateProfile);
-authRoutes.post("/auth/logout", logout);
+authRoutes.get("/auth/profile", authMiddleware, getProfile);
+authRoutes.put(
+  "/auth/profile",
+  authMiddleware,
+  updateProfileAuthValidations,
+  applyValidations,
+  updateProfile
+);
+authRoutes.post("/auth/logout", authMiddleware, logout);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,10 +6,19 @@ import {
   updateUser,
   deleteUser,
 } from "../controllers/user.controller.js";
-
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { adminMiddleware } from "../middlewares/admin.middleware.js";
 export const userRoutes = Router();
 
-userRoutes.put("/auth/users/:id", updateUser);
-userRoutes.get("/auth/users", getAllUsers);
-userRoutes.get("/auth/users/:id", getUserById);
-userRoutes.delete("/auth/users/:id", deleteUser);
+// userRoutes.use(authMiddleware);
+// userRoutes.use(adminMiddleware);
+
+userRoutes.put("/auth/users/:id", authMiddleware, adminMiddleware, updateUser);
+userRoutes.get("/auth/users", authMiddleware, adminMiddleware, getAllUsers);
+userRoutes.get("/auth/users/:id", authMiddleware, adminMiddleware, getUserById);
+userRoutes.delete(
+  "/auth/users/:id",
+  authMiddleware,
+  adminMiddleware,
+  deleteUser
+);
